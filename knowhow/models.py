364914675eeec3b5338ec6fc03c683ae19022089
System-out-gyuil <a46884334@gmail.com>
2024-02-29 12:51:15 +0900

    모델 수정

--- knowhow/models.py
+++ knowhow/models.py
@@ -1,30 +1,29 @@
 from django.db import models
 
+from selleaf.models import Period, File, Like, Plant, Scrap, Tag
+
 
 class Knowhow(Period):
     knowhow_title = models.CharField(max_length=50, null=False)
     knowhow_content = models.CharField(max_length=500, null=False)
     knowhow_count = models.IntegerField(default=0, null=False)
-    knowhow_category = models.ForeignKey('Category', on_delete=models.PROTECT)
-    user = models.Foreignkey('User', on_delete=models.PROTECT)
+    knowhow_category = models.CharField(null=False)
+    member = models.ForeignKey('Member', on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'tbl_knowhow'
 
-class KnowhowFile(models.Model):
-    # knowhow_file_id (슈퍼키)
+class KnowhowFile(File):
     knowhow = models.ForeignKey('Knowhow', on_delete=models.PROTECT)
     class Meta:
         db_table = 'tbl_knowhow_file'
 
-class KnowhowLike(models.Model):
-    # knowhow_like_id (슈퍼키)
+class KnowhowLike(Like):
     knowhow = models.ForeignKey('Knowhow', on_delete=models.PROTECT)
     class Meta:
         db_table = 'tbl_knowhow_like'
 
-class KnowhowPlant(Period):
-    plant = models.ForeignKey('PlantCategory', on_delete=models.PROTECT)
+class KnowhowPlant(Plant):
     knowhow = models.ForeignKey('Knowhow', on_delete=models.PROTECT)
     class Meta:
         db_table = 'tbl_knowhow_plant'
@@ -37,14 +36,26 @@ class KnowhowRecommend(models.Model):
 
 
 
-class KnowhowScrap(Period):
+class KnowhowScrap(Scrap):
     knowhow = models.ForeignKey('Knowhow', on_delete=models.PROTECT)
     class Meta:
         db_table = 'tbl_knowhow_scrap'
 
-class KnowhowTag(models.Model):
-    tag = models.ForeignKey('Tag', on_delete=models.PROTECT)
+class KnowhowTag(Tag):
     knowhow = models.ForeignKey('Knowhow', on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'tbl_knowhow_tag'
+
+class KnowhowReply(Period):
+    knowhow_reply_content = models.CharField(null=False, max_length=50)
+    knowhow = models.ForeignKey('Knowhow', on_delete=models.PROTECT)
+    member = models.ForeignKey('Member', on_delete=models.PROTECT)
+    class Meta:
+        db_table = 'tbl_knowhow_reply'
+
+class KnowhowReplyLike(Like):
+
+    knowhow_reply = models.ForeignKey('KnowhowReply', on_delete=models.PROTECT)
+    class Meta:
+        db_table = 'tbl_knowhow_reply_like'
