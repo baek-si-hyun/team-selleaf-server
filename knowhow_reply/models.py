e16eac76609e59c00f8b9eb3d2d45f49ca94b94e
System-out-gyuil <a46884334@gmail.com>
2024-02-29 09:51:39 +0900

    modelling

--- knowhow_reply/models.py
+++ knowhow_reply/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class KnowhowReply(Period):
+    knowhow_reply_content = models.CharField(null=False, max_length=50)
+    knowhow = models.ForeignKey('Knowhow', on_delete=models.PROTECT)
+    user = models.Foreignkey('User', on_delete=models.PROTECT)
+    class Meta:
+        db_table = 'tbl_knowhow_reply'
+
+class KnowhowReplyLike(models.Model):
+
+    knowhow_reply = models.ForeignKey('KnowhowReply', on_delete=models.PROTECT)
+    class Meta:
+        db_table = 'tbl_knowhow_reply_like'
--- post_reply/models.py
+++ post_reply/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class PostReply(models.Model):
+    post_reply_content = models.CharField(null=False, max_length=50)
+    post = models.ForeignKey('Post', on_delete=models.PROTECT)
+    user = models.Foreignkey('User', on_delete=models.PROTECT)
+    class Meta:
+        db_table = 'tbl_post_reply'
+
+class PostReplyLike(models.Model):
+
+    post_reply = models.ForeignKey('PostReply', on_delete=models.PROTECT)
+    class Meta:
+        db_table = 'tbl_post_reply_like'
