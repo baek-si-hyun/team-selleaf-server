054b40fd8b9a9e352962ce996368965e6852b1b3
DianaKang0123 <DianaKang0123@gmail.com>
2024-02-29 09:40:51 +0900

    240229 modeling in progress

--- mileage_history/models.py
+++ mileage_history/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from lecture_review.models import Review
+from selleaf.models import Mileage
+
+
+class OrderMileage(Mileage):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT,null=False, blank=False)
+
+    class Meta:
+        db_table = 'tbl_order_mileage'
+        ordering = ['-id']
+
+
+class ReviewMileage(Mileage):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
+    review = models.ForeignKey(Review, on_delete=models.PROTECT, null=False, blank=False)
+
+    class Meta:
+        db_table = 'tbl_review_mileage'
+        ordering = ['-id']
