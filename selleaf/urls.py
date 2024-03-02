bf427642d6fb7eecc68ad794265c79a74b6ef27b
baek-si-hyun <qortlgus100@gmail.com>
2024-03-02 15:37:48 +0900

    240302 add urls

--- selleaf/urls.py
+++ selleaf/urls.py
@@ -6,8 +6,21 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/', include('member.urls')),
+    path('lecture/', include('lecture.urls')),
+    path('order/', include('order.urls')),
+    path('teacher/', include('teacher.urls')),
+    path('trade/', include('trade.urls')),
+    path('notice/', include('notice.urls')),
+    path('qna/', include('qna.urls')),
+    path('report/', include('report.urls')),
+    path('knowhow/', include('knowhow.urls')),
+    path('post/', include('post.urls')),
+    path('cart/', include('cart.urls')),
+    path('alarm/', include('alarm.urls')),
+    path('accounts/', include('allauth.urls')),
     path('oauth/', include('oauth.urls')),
     path('accounts/', include('allauth.urls')),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
