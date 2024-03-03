049a307824ce9458512dfdebd1aa2120ac33575f
baek-si-hyun <qortlgus100@gmail.com>
2024-03-03 15:57:03 +0900

    240303 main templat edit Done

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,21 @@
+import random
+
 from django.test import TestCase
 
-# Create your tests here.
+from member.models import Member
+from post.models import Post
+
+
+class PostTest(TestCase):
+
+
+    member_queryset = Member.objects.all()
+
+    for i in range(50):
+        post = {
+            'post_title': f'게시물 제목{i}',
+            'post_content': f'게시물 내용{i}',
+            'member': member_queryset[random.randint(0, len(member_queryset)-1)],
+        }
+        Post.objects.create(**post)
+
