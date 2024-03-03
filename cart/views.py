6c4081ca9af42f1c2fc9371bda3541cc023484cd
DianaKang0123 <DianaKang0123@gmail.com>
2024-03-03 18:31:36 +0900

    cart views in progress

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
-
+# 장바구니 서비스
 class CartView(View):
+    # 장바구니 페이지로 이동
     def get(self,request):
         return render(request,'cart/cart.html')
+
+    def post(self):
+        pass
+
