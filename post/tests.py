018a3cfbf14a5a8a511b484662f28468dd11e518
baek-si-hyun <qortlgus100@gmail.com>
2024-03-03 18:28:42 +0900

    240303 migrations

--- post/tests.py
+++ post/tests.py
@@ -3,19 +3,23 @@ import random
 from django.test import TestCase
 
 from member.models import Member
-from post.models import Post
+from post.models import Post, PostFile
 
 
 class PostTest(TestCase):
-
-
     member_queryset = Member.objects.all()
-
-    for i in range(50):
-        post = {
+    for i in range(1, 50):
+        post_data = {
             'post_title': f'게시물 제목{i}',
             'post_content': f'게시물 내용{i}',
-            'member': member_queryset[random.randint(0, len(member_queryset)-1)],
+            'member': member_queryset[random.randint(0, len(member_queryset) - 1)],
         }
-        Post.objects.create(**post)
+        post = Post.objects.create(**post_data)
+
+        # PostFile 생성
+        post_file_data = {
+            'post': post,
+            'file_url': 'https://imagedelivery.net/4aEUbX05h6IovGOQjgkfSw/88ed654b-a983-4019-dff7-921f0c15d700/public'
+        }
+        PostFile.objects.create(**post_file_data)
 
