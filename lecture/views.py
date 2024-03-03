756a7e7804941955833b4e9da480cf3fde47176e
jonge5 <jonge54@gmail.com>
2024-03-03 13:59:30 +0900

    240303 lecture in progress

--- lecture/views.py
+++ lecture/views.py
@@ -5,3 +5,30 @@ from django.views import View
 class LectureView(View):
     def get(self, request):
         return render(request, 'lecture/web/lecture-main.html')
+
+
+class LectureDetailOnlineView(View):
+    def get(self, request):
+        return render(request, 'lecture/web/lecture-detail-online.html')
+
+
+class LectureDetailOfflineView(View):
+    def get(self, request):
+        return render(request, 'lecture/web/lecture-detail-offline.html')
+
+
+class LectureTotalView(View):
+    def get(self, request):
+        return render(request, 'lecture/web/lecture-total.html')
+
+
+class LectureUploadOnlineView(View):
+    def get(self, request):
+        return render(request, 'lecture/web/lecture-upload-online.html')
+
+
+class LectureUploadOfflineView(View):
+    def get(self, request):
+        return render(request, 'lecture/web/lecture-upload-offline.html')
+
+
