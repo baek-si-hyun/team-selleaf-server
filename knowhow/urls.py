495f879455d6d1a3fb309696fd10b45f4733fd84
System-out-gyuil <a46884334@gmail.com>
2024-03-03 18:31:03 +0900

    중간커밋

--- knowhow/urls.py
+++ knowhow/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
+from knowhow.views import KnowhowCreateView, KnowhowDetailView, KnowhowListView
 
 app_name = 'knowhow'
 
 urlpatterns = [
+    # 노하우 작성
+    path('create/', KnowhowCreateView.as_view(), name='create'),
+    # 노하우 상세
+    path('detail/', KnowhowDetailView.as_view(), name='detail'),
+    # 노하우 목록
+    path('list/', KnowhowListView.as_view(), name='list'),
 ]
--- knowhow/views.py
+++ knowhow/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.db import transaction
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from knowhow.models import Knowhow, KnowhowFile
+from member.models import Member
+
+
+class KnowhowCreateView(View):
+    def get(self, request):
+        return render(request, 'community/web/knowhow/create-knowhow.html')
+
+    @transaction.atomic
+    def post(self, request):
+        data = request.POST
+        # input 태그 하나 당 파일 1개일 때
+        file = request.FILES
+
+        # input 태그 하나에 여러 파일일 때(multiple), getlist('{input태그 name값}')
+        # files = request.FILES.getlist('upload-file')
+
+        member = Member(**request.session['member'])
+
+        # knowhowcategory = {
+        #
+        # }
+
+
+        knowhow = {
+            'knowhow_title': data['knowhow-title'],
+            'knowhow_content': data['knowhow-content'],
+            'member': member
+        }
+
+        knowhowplant = {
+            'plant_name': data['plant-name']
+        }
+
+        knowhowtag = {
+            'tag_name': data['knowhow-tags']
+        }
+
+
+
+
+        knowhowdata = Knowhow.objects.create(**knowhow)
+
+        # for file in files:
+        #     KnowhowFile.objects.create(post=knowhowdata, path=file)
+
+        for key in file:
+            KnowhowFile.objects.create(post=knowhowdata, path=file[key])
+
+        return redirect(post.get_absolute_url())
+class KnowhowDetailView(View):
+    pass
+
+class KnowhowListView(View):
+    pass
