c1975a1418f4d8e6a4be0f7a7d0538992f561239
NoviceKim <k7679568@naver.com>
2024-03-04 23:22:21 +0900

    notice view url changed

--- notice/views.py
+++ notice/views.py
@@ -5,4 +5,4 @@ from django.views import View
 # 공지사항 페이지 이동 뷰
 class NoticeWebView(View):
     def get(self, request):
-        return render(request, 'manager/manager-notice/manager-qna/manager-qna-compose.html')
+        return render(request, 'notice/web/notice.html')
