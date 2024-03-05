eefe65b0bf4c62e7e4539250a2ba5d005a356f3a
soo982181 <soo982181@naver.com>
2024-03-05 17:36:09 +0900

    trade/server in progress

--- trade/views.py
+++ trade/views.py
@@ -44,18 +44,6 @@ class TradeUploadView(View):
         # 오픈 채팅방 링크
         # trade_data['chatting-input']
 
-        # 사진파일
-        # 거래게시물등록 서비스에서는 사진을 최대 5장까지 받을 수 있음
-        # 하지만 1 ~ 5장까지 사용자가 몇장의 사진을 올렸는지는 모름 따라서
-        # 나는 이를 반복문을 통해 해결하였음
-        # realPhotos = []
-        # photos = [trade_data['img-file1'], trade_data['img-file2'], trade_data['img-file3'], trade_data['img-file4'], trade_data['img-file5']]
-        #
-        # for photo in photos:
-        #     if photo == '':
-        #         continue
-        #     realPhotos.append(photo)
-
         # 거래 게시글 제목
         # trade_data['title-input']
 
@@ -63,19 +51,23 @@ class TradeUploadView(View):
         # trade_data['content-input']
 
         # Trade create
-        data = {
-            'trade_price': trade_data['price-input'],
-            'trade_title': trade_data['title-input'],
-            'trade_content': trade_data['content-input'],
-            'member': Member.objects.get(id=member['id']),
-            'trade_category': TradeCategory.objects.create(category_name=trade_data['product-index']),
-            'kakao_talk_url': trade_data['chatting-input'],
-        }
+        # data = {
+        #     'trade_price': trade_data['price-input'],
+        #     'trade_title': trade_data['title-input'],
+        #     'trade_content': trade_data['content-input'],
+        #     'member': Member.objects.get(id=member['id']),
+        #     'trade_category': TradeCategory.objects.create(category_name=trade_data['product-index']),
+        #     'kakao_talk_url': trade_data['chatting-input'],
+        # }
 
-        trade = Trade.objects.create(**data)
+        # trade = Trade.objects.create(**data)
 
         # TradeFile create
-        for key in files:
-            TradeFile.objects.create(trade=trade, file_url=files[key])
+        # for key in files:
+            # TradeFile.objects.create(trade=trade, file_url=files[key])
 
+        # TradePlant
+        plant_types = trade_data.getlist('plant-type')
+        for plant_type in plant_types:
+            print(plant_type)
         return redirect('trade:detail')
