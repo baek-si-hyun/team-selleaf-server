e6d28546b3a0b5ba6c6d4eb0092219505874faab
System-out-gyuil <a46884334@gmail.com>
2024-03-05 09:01:45 +0900

    중간커밋

--- knowhow/views.py
+++ knowhow/views.py
@@ -2,7 +2,7 @@ from django.db import transaction
 from django.shortcuts import render, redirect
 from django.views import View
 
-from knowhow.models import Knowhow, KnowhowFile, KnowhowPlant, KnowhowTag
+from knowhow.models import Knowhow, KnowhowFile, KnowhowPlant, KnowhowTag, KnowhowCategory
 from member.models import Member
 
 
@@ -15,41 +15,42 @@ class KnowhowCreateView(View):
         data = request.POST
         file = request.FILES
 
-        print(data)
+        # print(data)
         # print(file)
 
         # 현재 로그인된 사람의 정보
-        # member = Member(**request.session['member'])
-        # print(member)
+        member = Member(**request.session['member'])
+        print(member)
 
-        # 아직 카테고리 테이블이 없음
+        # # 카테고리
         # knowhowcategory = {
-        #
+        #     'category_name': data['category-name']
         # }
-
-
-        # 노하우
+        #
+        #
+        # # 노하우
         # knowhow = {
         #     'knowhow_title': data['knowhow-title'],
         #     'knowhow_content': data['knowhow-content'],
         #     'member': member
         # }
-
-        # 노하우 식물분류
+        #
+        # # 노하우 식물분류
         # knowhowplant = {
         #     'plant_name': data['plant-name'],
         #     'knowhow':knowhow
         # }
-
-        # 노하우 태그
+        #
+        # # 노하우 태그
         # knowhowtag = {
         #     'tag_name': data['knowhow-tags'],
         #     'knowhow': knowhow
         # }
-
+        #
         # knowhowdata = Knowhow.objects.create(**knowhow)
         # KnowhowPlant.objects.create(**knowhowplant)
         # KnowhowTag.objects.create(**knowhowtag)
+        # KnowhowCategory.objects.create(**knowhowcategory)
         #
         for key in file:
             print('123')
