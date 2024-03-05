06890b15871fea7c9b0176a7288987884e8a3610
NoviceKim <k7679568@naver.com>
2024-03-05 19:58:12 +0900

    middleware optimization done

--- selleaf/middleware.py
+++ selleaf/middleware.py
@@ -18,8 +18,8 @@ def pre_handle_request(get_response):
         # 따라서 로그인이 필요한 서비스는 반드시 로그인 상태에서만 이용할 수 있게 됨
 
         # 03/04 - 조건식 수정 중
-        # 만약 요청한 경로가 아래 서비스들(관리자, 계정, oAuth, API) 중 그 어느 것도 아니면서
-        if 'admin' not in uri and 'accounts' not in uri and 'oauth' not in uri and 'api' not in uri:
+        # 만약 요청한 경로가 아래 서비스들(계정, oAuth, API) 중 그 어느 것도 아니면서
+        if 'accounts' not in uri and 'oauth' not in uri and 'api' not in uri:
             # 회원가입, 로그인 서비스도 아니고
             if 'join' not in uri and 'login' not in uri:
                 # 게시물 작성, 마이페이지, 강사 신청 서비스 중 하나일 때 - 03/04 추가
@@ -31,6 +31,16 @@ def pre_handle_request(get_response):
                         request.session['previous_uri'] = uri
                         # 로그인 페이지로 이동시킨다
                         return redirect('/member/login')
+
+                # 03/05 추가 - 관리자 페이지 요청
+                if 'admin' in uri:
+                    # 세션에 관리자 정보가 없으면
+                    if request.session.get('admin') is None:
+                        # 요청한 경로를 session에 담아놓은 뒤
+                        request.session['previous_uri'] = uri
+                        # 관리자 로그인 페이지로 이동시킨다
+                        return redirect('/member/login')
+
 
             # 모바일 환경에서 요청을 했지만
             if request.user_agent.is_mobile:
