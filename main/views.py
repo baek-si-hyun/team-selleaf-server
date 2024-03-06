d99fd2e569bd866c0ee27c03c007a24527037a86
baek-si-hyun <qortlgus100@gmail.com>
2024-03-06 16:29:04 +0900

    240306 migrations

--- main/views.py
+++ main/views.py
@@ -20,20 +20,24 @@ class MainView(View):
         return render(request, 'main/main.html', context)
 
 
-class MainAPI(APIView):
-    today = timezone.now().date()
-    start_of_week = today - timedelta(days=today.weekday())
-    end_of_week = start_of_week + timedelta(days=6)
+class MainKnowhowAPI(APIView):
+    def post(self, request):
+        today = timezone.now().date()
+        start_of_week = today - timedelta(days=today.weekday())
+        end_of_week = start_of_week + timedelta(days=6)
 
-    knowhows = Knowhow.objects.filter(created_date__range=(start_of_week, end_of_week)).order_by(
-        'knowhow_count')[:11]
+        knowhows = Knowhow.objects.filter(created_date__range=(start_of_week, end_of_week)).order_by(
+            'knowhow_count')[:11]
 
-    knowhow_serializers = KnowhowSerializer(knowhows, many=True)
+        knowhow_serializers = KnowhowSerializer(knowhows, many=True)
 
     # 강의 데이터 불러올 자리
 
-    start_of_day = datetime(today.year, today.month, today.day, 0, 0, 0)
-    end_of_day = datetime(today.year, today.month, today.day, 23, 59, 59)
 
-    trade = Trade.enabled_objects.filter(created_date__range=(start_of_day, end_of_day)).order_by('-id')
+class MainTradeAPI(APIView):
+    def post(self, request):
+        today = timezone.now().date()
+        start_of_day = datetime(today.year, today.month, today.day, 0, 0, 0)
+        end_of_day = datetime(today.year, today.month, today.day, 23, 59, 59)
 
+        trade = Trade.enabled_objects.filter(created_date__range=(start_of_day, end_of_day)).order_by('-id')
