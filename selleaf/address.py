42df7a2b3a343f6d5d026dd832e0b82bcbe8eaac
baek-si-hyun <qortlgus100@gmail.com>
2024-03-12 21:01:58 +0900

    240312 migration

--- selleaf/address.py
+++ selleaf/address.py
@@ -6,7 +6,7 @@ class Address(Period):
     address_zipcode = models.CharField(max_length=60, null=True, blank=False)
     address_city = models.CharField(max_length=255, null=False, blank=False)
     address_district = models.CharField(max_length=255, null=False, blank=False)
-    address_detail = models.CharField(max_length=255, null=False, blank=False)
+    address_detail = models.CharField(max_length=255, null=True, blank=False)
 
     class Meta:
         abstract = True
