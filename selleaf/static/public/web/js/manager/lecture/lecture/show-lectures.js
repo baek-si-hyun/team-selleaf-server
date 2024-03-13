b8feebdd5feb3df135c3a561cb9eb429dbe5bfe7
NoviceKim <k7679568@naver.com>
2024-03-13 14:55:55 +0900

    240313 lecture list read done

--- selleaf/static/public/web/js/manager/lecture/lecture/show-lectures.js
+++ selleaf/static/public/web/js/manager/lecture/lecture/show-lectures.js
@@ -1,13 +1,21 @@
 // 개설된 강의 목록을 화면에 띄우는 함수
-const showLecture = (lectures_info) => {
+const showLectures = (lectures_info) => {
     // HTML 코드를 담기 위한 빈 문자열
     let text = ``;
 
     // 받아온 데이터들 중 'lectures' 만 따로 가져옴
     const lectures = lectures_info.lectures;
 
+    // 수강료를 3자리 단위로 콤마(,)로 구분해주는 함수
+    const priceFormat = (price) => {
+        return new Intl.NumberFormat().format(price);
+    }
+
     // 받아온 회원 정보로 화면에 뿌릴 HTML 태그 생성
     lectures.forEach((lecture) => {
+        // 위 함수로 price에 3자리마다 콤마 붙임
+        let formattedPrice = priceFormat(lecture.lecture_price);
+
         text += `
                   <li class="list-content ${lecture.id}">
                     <input type="checkbox" class="checkbox-input" />
@@ -18,7 +26,7 @@ const showLecture = (lectures_info) => {
                       </div>
                       <div class="class-info">${lecture.teacher_name}</div>
                       <div class="class-info">${lecture.lecture_headcount}명</div>
-                      <div class="class-info">${lecture.lecture_price}원</div>
+                      <div class="class-info">${formattedPrice}원</div>
                       <div class="class-info">${lecture.lecture_place}</div>
                       <div class="class-info">${lecture.created_date}</div>
                     </a>
