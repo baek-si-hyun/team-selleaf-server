c53b5a9477e61a8c5315f13bc22194fc014b510c
NoviceKim <k7679568@naver.com>
2024-03-21 09:10:33 +0900

    setting updated

--- selleaf/settings.py
+++ selleaf/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-xrbh@fs#!56dnm^+1ld6l5n5t7n)1d@6t(@r3a&2!_x1j8r*$r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.38.116.70', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['3.38.116.70', '127.0.0.1', 'localhost', '52.79.151.112']
 
 # Application definition
 
