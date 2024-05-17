2768f28467043eb87e68a24066f4e8d76c1dfab0
baek-si-hyun <qortlgus100@gmail.com>
2024-05-17 20:56:43 +0900

    240517 knowhow,post models migration

--- knowhow/models.py
+++ knowhow/models.py
@@ -8,8 +8,8 @@ from selleaf.period import Period
 
 
 class Knowhow(Period):
-    knowhow_title = models.CharField(max_length=50, null=False)
-    knowhow_content = models.CharField(max_length=500, null=False)
+    knowhow_title = models.CharField(max_length=80, null=False)
+    knowhow_content = models.CharField(max_length=3000, null=False)
     knowhow_count = models.IntegerField(default=0, null=False)
     member = models.ForeignKey(Member, on_delete=models.PROTECT, null=False)
 
